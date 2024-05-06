9a5295538e0103b275304339d7429f0832dd8efd
anaaragon88 <ana.aragon88@gmail.com>
2024-05-06 14:32:19 +0200

    cambios posicion

--- main.js
+++ main.js
@@ -14,21 +14,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
+    // Crear una luz direccional
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Color blanco y intensidad 1
+    directionalLight.position.set(0, 10, 0); // Posición de la luz (arriba)
+    directionalLight.castShadow = true; // Habilitar la generación de sombras
+
+    directionalLight.shadow.mapSize.width = 1024;  // Ancho del mapa de sombras
+    directionalLight.shadow.mapSize.height = 1024; // Alto del mapa de sombras
+    directionalLight.shadow.camera.near = 0.5;     // Cercanía de la cámara de sombras
+    directionalLight.shadow.camera.far = 50;       // Lejanía de la cámara de sombras
+    scene.add(directionalLight);
+
+
     const oculus = await loadGLTF('./assets/1oculus_quest.glb');
-    oculus.scene.scale.set(4, 4, 4);
-    oculus.scene.position.set(0, -0.4, 0);
+    oculus.scene.scale.set(1, 1, 1);
+    oculus.scene.position.set(0, 0.5, 0);
 
     const smile = await loadGLTF('./assets/2smile.gltf');
     smile.scene.scale.set(10, 10, 10);
-    smile.scene.position.set(0, -0.5, 0);
+    smile.scene.position.set(0, 10, -1);
 
     const fb = await loadGLTF('./assets/3fb.gltf');
     fb.scene.scale.set(8, 8, 8);
     fb.scene.position.set(0, 0, 0);
+    fb.scene.rotation.set( 0,Math.PI / -2, 0); // Ajusta el ángulo de rotación según sea necesario
+
 
     const wa = await loadGLTF('./assets/4wa.gltf');
     wa.scene.scale.set(8, 8, 8);
     wa.scene.position.set(0, 0, 0);
+    fb.scene.rotation.set( Math.PI / 2,Math.PI / 2, 0);
 
     const ig = await loadGLTF('./assets/5ig.gltf');
     ig.scene.scale.set(8, 8, 8);
@@ -43,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
     metaiso.scene.position.set(0, -0.6, 0);
 
     const metalogo = await loadGLTF('./assets/8metalogo.gltf');
-    metalogo.scene.scale.set(8, 8, 8);
+    metalogo.scene.scale.set(4, 4, 4);
     metalogo.scene.position.set(0, -0.6, 0);
 
     const robot = await loadGLTF('./assets/9robot.glb');
@@ -51,7 +66,10 @@ document.addEventListener('DOMContentLoaded', () => {
     robot.scene.position.set(0, -1, 0);
     const robot2 = await loadGLTF('./assets/9robot.glb');
     robot2.scene.scale.set(0.5, 0.5, 0.5);
-    robot2.scene.position.set(1, -1, 1);
+    robot2.scene.position.set(1.2, -1, 1);
+    const robot3 = await loadGLTF('./assets/9robot.glb');
+    robot3.scene.scale.set(0.5, 0.5, 1);
+    robot3.scene.position.set(2.4, -1, 1);
 
 
     const oculus_anchor = mindarThree.addAnchor(0);
@@ -84,12 +102,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const robot2_anchor = mindarThree.addAnchor(8);
     robot2_anchor.group.add(robot2.scene);
 
+    const robot3_anchor = mindarThree.addAnchor(8);
+    robot3_anchor.group.add(robot3.scene);
+
     const mixer = new THREE.AnimationMixer(robot.scene);
     const action = mixer.clipAction(robot.animations[3]);
     action.play();
     const mixer2 = new THREE.AnimationMixer(robot2.scene);
-    const action2 = mixer2.clipAction(robot2.animations[3]);
+    const action2 = mixer2.clipAction(robot2.animations[6]);
     action2.play();
+    const mixer3 = new THREE.AnimationMixer(robot3.scene);
+    const action3 = mixer3.clipAction(robot3.animations[10]);
+    action3.play();
     /*await mindarThree.start();
     renderer.setAnimationLoop(() => {
 
@@ -99,15 +123,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const clock = new THREE.Clock();
 
     await mindarThree.start();
+
     renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
       oculus.scene.rotation.set(0, oculus.scene.rotation.y + delta, 0);
-      fb.scene.rotation.set(0, fb.scene.rotation.z + delta, 0);
+      pride.scene.rotation.set(0, pride.scene.rotation.y + delta, 0);
+      metalogo.scene.rotation.set(0, metalogo.scene.rotation.y + delta, 0);
 
-      robot.scene.rotation.set(0, robot.scene.rotation.y + delta, 0);
-      mixer.update(delta);
-      robot2.scene.rotation.set(0, robot2.scene.rotation.y + delta, 0);
-      mixer2.update(delta);
+
+      //robot.scene.rotation.set(0, robot.scene.rotation.y + delta, 0);
+     mixer.update(delta);
+      //robot2.scene.rotation.set(0, robot2.scene.rotation.y + delta, 0);
+    mixer2.update(delta);
+    mixer3.update(delta);
       renderer.render(scene, camera);
     });
 
